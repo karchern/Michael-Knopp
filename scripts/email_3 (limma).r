90ef3dc6cc2586244fd383a3a532594680521399
Nicolai Karcher <karchernic@gmail.com>
2025-11-02 16:47:11 +0100

    fixing bug in email_3 (limma)

--- scripts/email_3 (limma).r
+++ scripts/email_3 (limma).r
@@ -31,11 +31,11 @@ for (comp in comparisons) {
     tmp <- limma %>%
         filter(comparison == comp)
     p <- ggplot() +
-        geom_point(data = tmp %>% filter(!hit), aes(x = logFC, y = -log10(P.Value)), color = "grey", alpha = 0.1) +
-        geom_point(data = tmp %>% filter(hit), aes(x = logFC, y = -log10(P.Value)), color = "red", alpha = 0.5) +
+        geom_point(data = tmp %>% filter(!hit), aes(x = logFC, y = -log10(adj.P.Val)), color = "grey", alpha = 0.1) +
+        geom_point(data = tmp %>% filter(hit), aes(x = logFC, y = -log10(adj.P.Val)), color = "red", alpha = 0.5) +
         geom_text_repel(
             data = tmp %>% filter(hit),
-            aes(x = logFC, y = -log10(P.Value), label = gene_name),
+            aes(x = logFC, y = -log10(adj.P.Val), label = gene_name),
             size = 3,
             max.overlaps = Inf
         ) +
