f37b52f3564bdff1f4cc0576d7ae9a6b06c64edb
Nicolai Karcher <karchernic@gmail.com>
2025-11-04 16:31:26 +0100

    updating email_2 (knockouts)

--- scripts/email_2 (knockouts).r
+++ scripts/email_2 (knockouts).r
@@ -9,17 +9,28 @@ library(readxl)
 
 source(here("scripts", "utils.r"))
 
-knockouts <- read_excel("/Users/karcher/Michael-Knopp/data/knockouts.xlsx")
-p1 <- ggplot(data = knockouts) +
-    geom_boxplot(aes(
-        x = community,
-        y = selection_coefficient,
-        fill = strain
-    )) +
-    theme_presentation()
+knockouts <- read_excel("/Users/karcher/Michael-Knopp/data/knockouts.xlsx") %>%
+    mutate(strain = factor(strain, levels = unique(strain)))
 
-ggsave(
-    plot = p1,
-    filename = here("results/plots", "supplementary_figure_XX_knockouts_selection_coefficients.pdf"),
-    width = 8, height = 4
-)
+for (com in unique(knockouts$community)) {
+    tmp <- knockouts %>%
+        filter(community == com)
+    p1 <- ggplot() +
+        geom_boxplot(
+            data = tmp,
+            aes(
+                x = strain,
+                y = selection_coefficient,
+            )
+        ) +
+        theme_presentation() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+        ylab("Selection Coefficient") +
+        xlab("Strain")
+
+    ggsave(
+        plot = p1,
+        filename = here("results/plots", str_c("supplementary_figure_XX_knockouts_selection_coefficients_", com, ".pdf")),
+        width = 4, height = 4
+    )
+}
